75c4718eb78b33d373cf83fd47b8ffbd4a3be9eb
xen <175407698+kayikc@users.noreply.github.com>
2025-10-28 13:38:38 +1100

    Build first route Home

---
+++ src/routes/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p1>This is me building my second page!</p1>
+      <ul>
+        <li>first point</li>
+        <li>second point</li>
+        <li>third point</li>
+      </ul>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/routes/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>My Task 1</h1>
+      <p>This is my first build!</p>
+    </div>
+  );
+}
+
+export default Home;
