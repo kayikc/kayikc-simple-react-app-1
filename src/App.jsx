aaee1a84478dac5f96f657233fe14114b0001c86
xen <175407698+kayikc@users.noreply.github.com>
2025-10-28 13:50:59 +1100

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+
 function App() {
   return (
-    <div>
-      <h1>my project 1!</h1>
-      <p>Howdy!</p>
-    </div>
+    <BrowserRouter>
+      <div style={{ paddig: "5px", fontFamily: "consola" }}>
+        {/* Nav*/}
+        <nav
+          style={{
+            marginBottom: "20px",
+            padding: "10px",
+            backgroundColor: "#f0f0f0",
+            borderRadius: "8px",
+          }}
+        >
+          <Link to="/" style={{ marginRight: "10px" }}>
+            Home
+          </Link>
+          <Link to="/about">About</Link>
+        </nav>
+
+        {/*Routes*/}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
